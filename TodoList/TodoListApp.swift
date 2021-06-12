034e3347690b9ff9f6898f693439fcb3b0f4a186
YossiBenZaken <yossibz@gmail.com>
2021-06-12 20:42:04 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yosef Ben Zaken on 12/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
